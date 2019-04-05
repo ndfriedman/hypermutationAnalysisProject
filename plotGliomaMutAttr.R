aebf653c85e27a0de5f7bdd460385867955293c1
NoahFriedman <friedman@mskcc.org>
2019-04-05 11:24:02 -0400

    remove

--- plotGliomaMutAttr.R
+++
@@ -1,9 +0,0 @@
-#written by noah friedman
-#a template for R scripts for plotting
-
-library(ggplot2)
-library(grid)
-require(cowplot)
-library(egg)
-
-if(!exists("foo", mode="function")) source("/Users/friedman/Desktop/mnt/ifs/work/taylorlab/friedman/myUtils/landscape_plot_util.R")
