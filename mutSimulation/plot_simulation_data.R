bc8ae9af2af1f33f1b8d98e45273d79b10a811ab
NoahFriedman <friedman@mskcc.org>
2019-04-05 11:36:06 -0400

    changes to plotting of simulation data

--- mutSimulation/plot_simulation_data.R
+++ mutSimulation/plot_simulation_data.R
@@ -9,17 +9,12 @@ library(egg)
 if(!exists("foo", mode="function")) source("/Users/friedman/Desktop/mnt/ifs/work/taylorlab/friedman/myUtils/landscape_plot_util.R")
 
 plotSimulationBarData <- function(df){
-  plt <- ggplot(df, aes(x= reorder(factor(SigName), average), y=average,  fill = factor(modelName))) +
-    geom_bar(stat='identity', position = "dodge2")+
-    theme(axis.text.x = element_text(angle = 60, hjust = 1, size=5))+
-    ylab('N Oncogenic mutations per non oncogenic mutation')+
-    xlab('Signature')+
-    ggtitle('Simulated Acquisition of Oncogenic Mutations by Signature')
+  
   return(plt)
 }
 
 
-simulationData <- read.table('~/Desktop/dataForLocalPlotting/simulationData.tsv',sep = '\t', header=TRUE) #load a dataframe that has already been formatted properly by a python util
+simulationData <- read.table('~/Desktop/WORK/dataForLocalPlotting/simulationData.tsv',sep = '\t', header=TRUE) #load a dataframe that has already been formatted properly by a python util
 
 plt<- plotSimulationBarData(simulationData)
 
