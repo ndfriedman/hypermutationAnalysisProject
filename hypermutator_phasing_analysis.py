1e759c53e0b1739e402a1293fc692296deb6d17e
NoahFriedman <friedman@mskcc.org>
2019-04-05 11:34:41 -0400

    adjusted phasing info

--- hypermutator_phasing_analysis.py
+++ hypermutator_phasing_analysis.py
@@ -16,18 +16,127 @@ import analysis_utils
 import mutationSigUtils 
 import maf_analysis_utils
 
-phasingData = pd.read_table(pathPrefix + '/home/ang46/luna/projects/collaborations/noah/phased_samples_for_noah_20190116.txt')
+
+def enumerate_recurrently_mutated_tumor_supressors_and_oncogenes(cohortMaf, thresh=.1):
+    tumorSupressors = set(['ERRFI1', 'ASXL2', 'PMAIP1', 'ACTG1', 'SUFU', 'FBXO11', 'MEN1', 'FAM58A', 'B2M', 'RB1', 'DUSP22', 'SESN1', 'GPS2', 'RAD51D', 'SMG1', 'CDC73', 'MAP3K1', 'SMARCB1', 'INPP4B', 'PARK2', 'SMAD4', 'CBFB', 'CDH1', 'PPP6C', 'SETDB1', 'SETDB2', 'NF2', 'CDKN2B', 'CDKN2C', 'CDKN2A', 'DDX3X', 'PIK3R1', 'BARD1', 'PDS5B', 'KLF4', 'SPRED1', 'VHL', 'SMAD2', 'PMS1', 'PMS2', 'SETD2', 'GATA3', 'TBL1XR1', 'MUTYH', 'SOCS1', 'FAM175A', 'ROBO1', 'ARID1B', 'ARID1A', 'TCF7L2', 'STK11', 'FOXA1', 'PTEN', 'FAT1', 'FAS', 'CYLD', 'MAX', 'SH2D1A', 'APC', 'NTHL1', 'CTCF', 'KDM5C', 'KMT2C', 'ZFHX3', 'FOXP1', 'PIGA', 'CDKN1B', 'CDKN1A', 'FUBP1', 'MSH2', 'ID3', 'TNFRSF14', 'TRAF3', 'EP400', 'BRIP1', 'ARID4A', 'ARID4B', 'XRCC2', 'DAXX', 'SDHAF2', 'ASXL1', 'AMER1', 'RASA1', 'EGR1', 'MST1', 'SOX17', 'RUNX1', 'PIK3R3', 'NCOR1', 'NF1', 'JAK1', 'PTPRD', 'CHEK2', 'CHEK1', 'SMC1A', 'TMEM127', 'STAG1', 'RAD51', 'TCF3', 'STAG2', 'ARID2', 'RAD50', 'RNF43', 'PARP1', 'BLM', 'CUX1', 'RECQL', 'RAD21', 'PTPN2', 'PTPN1', 'SLX4', 'INHA', 'PAX5', 'IRF1', 'TP53', 'HLA-A', 'IRF8', 'CBL', 'TOP1', 'SHQ1', 'PRDM1', 'NSD1', 'ATXN2', 'CREBBP', 'HDAC4', 'SESN2', 'PPP2R1A', 'EPHA7', 'ATM', 'EPHA3', 'POT1', 'SMAD3', 'MOB3B', 'TBX3', 'POLE', 'ATR', 'FANCD2', 'FH', 'BCORL1', 'SOX9', 'IKZF3', 'TSC1', 'TP63', 'MRE11A', 'SDHC', 'BTG1', 'POLD1', 'CIITA', 'SMC3', 'SAMHD1', 'RTEL1', 'ECT2L', 'PIK3R2', 'CRBN', 'FANCC', 'NBN', 'FANCA', 'HLA-B', 'RECQL4', 'DUSP4', 'ERCC2', 'FBXW7', 'TGFBR2', 'TGFBR1', 'MSH3', 'RBM15', 'TET1', 'TET3', 'SESN3', 'MGA', 'LTB', 'FOXL2', 'SH2B3', 'BCOR', 'HIST1H1D', 'ATRX', 'EP300', 'RAD51C', 'RAD51B', 'HIST1H1B', 'TNFAIP3', 'DICER1', 'ARID5B', 'LATS2', 'FOXO1', 'KEAP1', 'EZH2', 'SP140', 'NKX3-1', 'PBRM1', 'PALB2', 'CIC', 'BRCA1', 'DTX1', 'FLCN', 'SPEN', 'CD58', 'ERCC3', 'ERCC4', 'MSH6', 'BCL11B', 'BMPR1A', 'ERF', 'BRCA2', 'NOTCH2', 'EED', 'MITF', 'ELF3', 'SMARCA4', 'BBC3', 'ANKRD11', 'CEBPA', 'BCL2L11', 'AXIN2', 'AXIN1', 'CDK12', 'ESCO2', 'MLH1', 'SDHB', 'MED12', 'HNF1A', 'RYBP', 'ATP6V1B2', 'DNMT3B', 'KMT2B', 'KMT2A', 'DNMT3A', 'NFKBIA', 'TRAF5', 'KMT2D', 'SPOP', 'RBM10', 'P2RY8', 'TP53BP1', 'TSC2', 'KDM6A', 'EPCAM', 'PHOX2B', 'NPM1', 'BCL10', 'LATS1', 'HOXB13', 'ARID3A', 'PTPRT', 'PTPRS', 'INPPL1', 'NOTCH4', 'TET2', 'NOTCH1', 'CASP8', 'NOTCH3', 'GRIN2A', 'MAP2K4', 'WT1', 'BACH2', 'SDHA', 'BAP1', 'PTCH1', 'SDHD'])
+    occurenceCounter, rankingDict, fractionalDict = maf_analysis_utils.enumerate_top_n_oncogenic_mutated_genes_across_cohort(cohortMaf, n=50)
+    recurrentTumorSupressors = []
+    recurrentOncogenes = []
+    for key, value in fractionalDict.items():
+        if value > .1:
+            if key in tumorSupressors:
+                recurrentTumorSupressors.append(key)
+            else:
+                recurrentOncogenes.append(key) 
+    return set(recurrentTumorSupressors), set(recurrentOncogenes)
+
+#creates summaries of pahsing data across four categories by cancer type
+def analyze_phasing_amongst_mutations_in_cancer_type(cancerTypeIds, maf, phasingData, cancerType):
+    
+    genes = set(['ABL1', 'ACVR1', 'AGO2', 'AKT1', 'AKT2', 'AKT3', 'ALK', 'ALOX12B', 'ANKRD11', 'APC', 'AR', 'ARAF', 'ARID1A', 'ARID1B', 'ARID2', 'ARID5B', 'ASXL1', 'ASXL2', 'ATM', 'ATR', 'ATRX', 'AURKA', 'AURKB', 'AXIN1', 'AXIN2', 'AXL', 'B2M', 'BABAM1', 'BAP1', 'BARD1', 'BBC3', 'BCL10', 'BCL2', 'BCL2L1', 'BCL2L11', 'BCL6', 'BCOR', 'BIRC3', 'BLM', 'BMPR1A', 'BRAF', 'BRCA1', 'BRCA2', 'BRD4', 'BRIP1', 'BTK', 'CALR', 'CARD11', 'CARM1', 'CASP8', 'CBFB', 'CBL', 'CCND1', 'CCND2', 'CCND3', 'CCNE1', 'CD274', 'CD276', 'CD79A', 'CD79B', 'CDC42', 'CDC73', 'CDH1', 'CDK12', 'CDK4', 'CDK6', 'CDK8', 'CDKN1A', 'CDKN1B', 'CDKN2A', 'CDKN2B', 'CDKN2C', 'CEBPA', 'CENPA', 'CHEK1', 'CHEK2', 'CIC', 'CREBBP', 'CRKL', 'CRLF2', 'CSDE1', 'CSF1R', 'CSF3R', 'CTCF', 'CTLA4', 'CTNNB1', 'CUL3', 'CXCR4', 'CYLD', 'CYSLTR2', 'DAXX', 'DCUN1D1', 'DDR2', 'DICER1', 'DIS3', 'DNAJB1', 'DNMT1', 'DNMT3A', 'DNMT3B', 'DOT1L', 'DROSHA', 'DUSP4', 'E2F3', 'EED', 'EGFL7', 'EGFR', 'EIF1AX', 'EIF4A2', 'EIF4E', 'ELF3', 'EP300', 'EPAS1', 'EPCAM', 'EPHA3', 'EPHA5', 'EPHA7', 'EPHB1', 'ERBB2', 'ERBB3', 'ERBB4', 'ERCC2', 'ERCC3', 'ERCC4', 'ERCC5', 'ERF', 'ERG', 'ERRFI1', 'ESR1', 'ETV1', 'ETV6', 'EZH1', 'EZH2', 'FAM123B', 'FAM175A', 'FAM46C', 'FAM58A', 'FANCA', 'FANCC', 'FAT1', 'FBXW7', 'FGF19', 'FGF3', 'FGF4', 'FGFR1', 'FGFR2', 'FGFR3', 'FGFR4', 'FH', 'FLCN', 'FLT1', 'FLT3', 'FLT4', 'FOXA1', 'FOXL2', 'FOXO1', 'FOXP1', 'FUBP1', 'FYN', 'GATA1', 'GATA2', 'GATA3', 'GLI1', 'GNA11', 'GNAQ', 'GNAS', 'GPS2', 'GREM1', 'GRIN2A', 'GSK3B', 'H3F3A', 'H3F3B', 'H3F3C', 'HGF', 'HIST1H1C', 'HIST1H2BD', 'HIST1H3A', 'HIST1H3B', 'HIST1H3C', 'HIST1H3D', 'HIST1H3E', 'HIST1H3F', 'HIST1H3G', 'HIST1H3H', 'HIST1H3I', 'HIST1H3J', 'HIST2H3C', 'HIST2H3D', 'HIST3H3', 'HLA-A', 'HLA-B', 'HNF1A', 'HOXB13', 'HRAS', 'ICOSLG', 'ID3', 'IDH1', 'IDH2', 'IFNGR1', 'IGF1', 'IGF1R', 'IGF2', 'IKBKE', 'IKZF1', 'IL10', 'IL7R', 'INHA', 'INHBA', 'INPP4A', 'INPP4B', 'INPPL1', 'INSR', 'IRF4', 'IRS1', 'IRS2', 'JAK1', 'JAK2', 'JAK3', 'JUN', 'KDM5A', 'KDM5C', 'KDM6A', 'KDR', 'KEAP1', 'KIT', 'KLF4', 'KMT2B', 'KMT5A', 'KNSTRN', 'KRAS', 'LATS1', 'LATS2', 'LMO1', 'LYN', 'MALT1', 'MAP2K1', 'MAP2K2', 'MAP2K4', 'MAP3K1', 'MAP3K13', 'MAP3K14', 'MAPK1', 'MAPK3', 'MAPKAP1', 'MAX', 'MCL1', 'MDC1', 'MDM2', 'MDM4', 'MED12', 'MEF2B', 'MEN1', 'MET', 'MGA', 'MITF', 'MLH1', 'MLL', 'MLL2', 'MLL3', 'MPL', 'MRE11A', 'MSH2', 'MSH3', 'MSH6', 'MSI1', 'MSI2', 'MST1', 'MST1R', 'MTOR', 'MUTYH', 'MYC', 'MYCL1', 'MYCN', 'MYD88', 'MYOD1', 'NBN', 'NCOA3', 'NCOR1', 'NEGR1', 'NF1', 'NF2', 'NFE2L2', 'NFKBIA', 'NKX2-1', 'NKX3-1', 'NOTCH1', 'NOTCH2', 'NOTCH3', 'NOTCH4', 'NPM1', 'NRAS', 'NSD1', 'NTHL1', 'NTRK1', 'NTRK2', 'NTRK3', 'NUF2', 'NUP93', 'PAK1', 'PAK7', 'PALB2', 'PARK2', 'PARP1', 'PAX5', 'PBRM1', 'PDCD1', 'PDCD1LG2', 'PDGFRA', 'PDGFRB', 'PDPK1', 'PGR', 'PHOX2B', 'PIK3C2G', 'PIK3C3', 'PIK3CA', 'PIK3CB', 'PIK3CD', 'PIK3CG', 'PIK3R1', 'PIK3R2', 'PIK3R3', 'PIM1', 'PLCG2', 'PLK2', 'PMAIP1', 'PMS1', 'PMS2', 'PNRC1', 'POLD1', 'POLE', 'PPARG', 'PPM1D', 'PPP2R1A', 'PPP4R2', 'PPP6C', 'PRDM1', 'PRDM14', 'PREX2', 'PRKAR1A', 'PRKCI', 'PRKD1', 'PTCH1', 'PTEN', 'PTP4A1', 'PTPN11', 'PTPRD', 'PTPRS', 'PTPRT', 'RAB35', 'RAC1', 'RAC2', 'RAD21', 'RAD50', 'RAD51', 'RAD51C', 'RAD51L1', 'RAD51L3', 'RAD52', 'RAD54L', 'RAF1', 'RARA', 'RASA1', 'RB1', 'RBM10', 'RECQL', 'RECQL4', 'REL', 'RET', 'RFWD2', 'RHEB', 'RHOA', 'RICTOR', 'RIT1', 'RNF43', 'ROS1', 'RPS6KA4', 'RPS6KB2', 'RPTOR', 'RRAGC', 'RRAS', 'RRAS2', 'RTEL1', 'RUNX1', 'RXRA', 'RYBP', 'SDHA', 'SDHAF2', 'SDHB', 'SDHC', 'SDHD', 'SESN1', 'SESN2', 'SESN3', 'SETD2', 'SF3B1', 'SH2B3', 'SH2D1A', 'SHOC2', 'SHQ1', 'SLX4', 'SMAD2', 'SMAD3', 'SMAD4', 'SMARCA4', 'SMARCB1', 'SMARCD1', 'SMO', 'SMYD3', 'SOCS1', 'SOS1', 'SOX17', 'SOX2', 'SOX9', 'SPEN', 'SPOP', 'SPRED1', 'SRC', 'SRSF2', 'STAG2', 'STAT3', 'STAT5A', 'STAT5B', 'STK11', 'STK19', 'STK40', 'SUFU', 'SUZ12', 'SYK', 'TAP1', 'TAP2', 'TBX3', 'TCEB1', 'TCF3', 'TCF7L2', 'TEK', 'TERT', 'TET1', 'TET2', 'TGFBR1', 'TGFBR2', 'TMEM127', 'TMPRSS2', 'TNFAIP3', 'TNFRSF14', 'TOP1', 'TP53', 'TP53BP1', 'TP63', 'TRAF2', 'TRAF7', 'TSC1', 'TSC2', 'TSHR', 'U2AF1', 'UPF1', 'VEGFA', 'VHL', 'VTCN1', 'WHSC1', 'WHSC1L1', 'WT1', 'WWTR1', 'XIAP', 'XPO1', 'XRCC2', 'YAP1', 'YES1', 'ZFHX3', 'ZRSR2'])
+    tumorSupressors = set(['ERRFI1', 'ASXL2', 'PMAIP1', 'ACTG1', 'SUFU', 'FBXO11', 'MEN1', 'FAM58A', 'B2M', 'RB1', 'DUSP22', 'SESN1', 'GPS2', 'RAD51D', 'SMG1', 'CDC73', 'MAP3K1', 'SMARCB1', 'INPP4B', 'PARK2', 'SMAD4', 'CBFB', 'CDH1', 'PPP6C', 'SETDB1', 'SETDB2', 'NF2', 'CDKN2B', 'CDKN2C', 'CDKN2A', 'DDX3X', 'PIK3R1', 'BARD1', 'PDS5B', 'KLF4', 'SPRED1', 'VHL', 'SMAD2', 'PMS1', 'PMS2', 'SETD2', 'GATA3', 'TBL1XR1', 'MUTYH', 'SOCS1', 'FAM175A', 'ROBO1', 'ARID1B', 'ARID1A', 'TCF7L2', 'STK11', 'FOXA1', 'PTEN', 'FAT1', 'FAS', 'CYLD', 'MAX', 'SH2D1A', 'APC', 'NTHL1', 'CTCF', 'KDM5C', 'KMT2C', 'ZFHX3', 'FOXP1', 'PIGA', 'CDKN1B', 'CDKN1A', 'FUBP1', 'MSH2', 'ID3', 'TNFRSF14', 'TRAF3', 'EP400', 'BRIP1', 'ARID4A', 'ARID4B', 'XRCC2', 'DAXX', 'SDHAF2', 'ASXL1', 'AMER1', 'RASA1', 'EGR1', 'MST1', 'SOX17', 'RUNX1', 'PIK3R3', 'NCOR1', 'NF1', 'JAK1', 'PTPRD', 'CHEK2', 'CHEK1', 'SMC1A', 'TMEM127', 'STAG1', 'RAD51', 'TCF3', 'STAG2', 'ARID2', 'RAD50', 'RNF43', 'PARP1', 'BLM', 'CUX1', 'RECQL', 'RAD21', 'PTPN2', 'PTPN1', 'SLX4', 'INHA', 'PAX5', 'IRF1', 'TP53', 'HLA-A', 'IRF8', 'CBL', 'TOP1', 'SHQ1', 'PRDM1', 'NSD1', 'ATXN2', 'CREBBP', 'HDAC4', 'SESN2', 'PPP2R1A', 'EPHA7', 'ATM', 'EPHA3', 'POT1', 'SMAD3', 'MOB3B', 'TBX3', 'POLE', 'ATR', 'FANCD2', 'FH', 'BCORL1', 'SOX9', 'IKZF3', 'TSC1', 'TP63', 'MRE11A', 'SDHC', 'BTG1', 'POLD1', 'CIITA', 'SMC3', 'SAMHD1', 'RTEL1', 'ECT2L', 'PIK3R2', 'CRBN', 'FANCC', 'NBN', 'FANCA', 'HLA-B', 'RECQL4', 'DUSP4', 'ERCC2', 'FBXW7', 'TGFBR2', 'TGFBR1', 'MSH3', 'RBM15', 'TET1', 'TET3', 'SESN3', 'MGA', 'LTB', 'FOXL2', 'SH2B3', 'BCOR', 'HIST1H1D', 'ATRX', 'EP300', 'RAD51C', 'RAD51B', 'HIST1H1B', 'TNFAIP3', 'DICER1', 'ARID5B', 'LATS2', 'FOXO1', 'KEAP1', 'EZH2', 'SP140', 'NKX3-1', 'PBRM1', 'PALB2', 'CIC', 'BRCA1', 'DTX1', 'FLCN', 'SPEN', 'CD58', 'ERCC3', 'ERCC4', 'MSH6', 'BCL11B', 'BMPR1A', 'ERF', 'BRCA2', 'NOTCH2', 'EED', 'MITF', 'ELF3', 'SMARCA4', 'BBC3', 'ANKRD11', 'CEBPA', 'BCL2L11', 'AXIN2', 'AXIN1', 'CDK12', 'ESCO2', 'MLH1', 'SDHB', 'MED12', 'HNF1A', 'RYBP', 'ATP6V1B2', 'DNMT3B', 'KMT2B', 'KMT2A', 'DNMT3A', 'NFKBIA', 'TRAF5', 'KMT2D', 'SPOP', 'RBM10', 'P2RY8', 'TP53BP1', 'TSC2', 'KDM6A', 'EPCAM', 'PHOX2B', 'NPM1', 'BCL10', 'LATS1', 'HOXB13', 'ARID3A', 'PTPRT', 'PTPRS', 'INPPL1', 'NOTCH4', 'TET2', 'NOTCH1', 'CASP8', 'NOTCH3', 'GRIN2A', 'MAP2K4', 'WT1', 'BACH2', 'SDHA', 'BAP1', 'PTCH1', 'SDHD'])
+    oncogenes = genes - tumorSupressors
+    
+    cancerTypeMaf = maf[maf['Tumor_Sample_Barcode'].isin(cancerTypeIds)]
+    recurrentTumorSupressors, recurrentOncogenes = enumerate_recurrently_mutated_tumor_supressors_and_oncogenes(cancerTypeMaf)
+    nonRecurrentTumorSupressors = tumorSupressors - recurrentTumorSupressors
+    nonRecurrentOncogenes = oncogenes - recurrentOncogenes
+    
+    #phasingData = phasingData[phasingData['oncogenic.1'].notnull()]
+    phasingCancerType = phasingData[phasingData['Tumor_Sample_Barcode'].isin(cancerTypeIds)]
+    
+    recurrentTumorSupressorMuts = phasingCancerType[phasingCancerType['Hugo_Symbol'].isin(recurrentTumorSupressors)]
+    recurrentOnocgeneMuts = phasingCancerType[phasingCancerType['Hugo_Symbol'].isin(recurrentOncogenes)]
+    nonRecurrentTumorSupressorMuts = phasingCancerType[phasingCancerType['Hugo_Symbol'].isin(nonRecurrentTumorSupressors)]
+    nonRecurrentOncogenesMuts = phasingCancerType[phasingCancerType['Hugo_Symbol'].isin(nonRecurrentOncogenes)]
+    
+    cntrRTSMuts = Counter(recurrentTumorSupressorMuts['phase'])
+    cntrROMuts = Counter(recurrentOnocgeneMuts['phase'])
+    cntrNRTSMuts = Counter(nonRecurrentTumorSupressorMuts['phase'])
+    cntrNROMuts = Counter(nonRecurrentOncogenesMuts['phase'])
+    
+    l =[
+        {'cancer_type': cancerType,
+        'label': 'cis',
+        'geneLabel': 'RecurrentTumorSupressor',
+        'frac': 1.0*(cntrRTSMuts['cis'])/recurrentTumorSupressorMuts.shape[0]},
+        {'cancer_type': cancerType,
+        'label': 'cis',
+        'geneLabel': 'NonRecurrentTumorSupressor',
+        'frac': 1.0*(cntrNRTSMuts['cis'])/nonRecurrentTumorSupressorMuts.shape[0]},
+         {'cancer_type': cancerType,
+        'label': 'cis',
+        'geneLabel': 'RecurrentOncogene',
+        'frac': 1.0*(cntrROMuts['cis'])/recurrentOnocgeneMuts.shape[0]},
+        {'cancer_type': cancerType,
+        'label': 'cis',
+        'geneLabel': 'NonRecurrentOncogene',
+        'frac': 1.0*(cntrNROMuts['cis'])/nonRecurrentOncogenesMuts.shape[0]},
+        
+        {'cancer_type': cancerType,
+        'label': 'trans',
+        'geneLabel': 'RecurrentTumorSupressor',
+        'frac': 1.0*(cntrRTSMuts['trans'] + cntrRTSMuts['trans or separate cells'])/recurrentTumorSupressorMuts.shape[0]},
+        {'cancer_type': cancerType,
+        'label': 'trans',
+        'geneLabel': 'NonRecurrentTumorSupressor',
+        'frac': 1.0*(cntrNRTSMuts['trans'] + cntrNRTSMuts['trans or separate cells'])/nonRecurrentTumorSupressorMuts.shape[0]},
+        {'cancer_type': cancerType,
+        'label': 'trans',
+        'geneLabel': 'RecurrentOncogene',
+        'frac': 1.0*(cntrROMuts['trans'] + cntrROMuts['trans or separate cells'])/recurrentOnocgeneMuts.shape[0]},
+        {'cancer_type': cancerType,
+        'label': 'trans',
+        'geneLabel': 'NonRecurrentOncogene',
+        'frac': 1.0*(cntrNROMuts['trans'] + cntrNROMuts['trans or separate cells'])/nonRecurrentOncogenesMuts.shape[0]}
+        ]
+    
+    return l
+    
+phasingData = pd.read_table(pathPrefix +'/ifs/res/taylorlab/ang46/data/multiplets/original_data/data_merged_prepped_datafreeze_phased.txt')
 
 
-phasingData.columns.values
-phasingData['oncogenic.2']
+print phasingData[(phasingData['Tumor_Sample_Barcode'] == 'P-0015885-T01-IM6') & (phasingData['Hugo_Symbol'] == 'KRAS')]['phase']
+
+
+print phasingData[(phasingData['Tumor_Sample_Barcode'] == 'P-0012358-T01-IM5')]['purity']
+print phasingData[(phasingData['Tumor_Sample_Barcode'] == 'P-0012358-T01-IM5')]['ploidy']
+print phasingData[(phasingData['Tumor_Sample_Barcode'] == 'P-0012358-T01-IM5')]['tcn.em.1']
+print phasingData[(phasingData['Tumor_Sample_Barcode'] == 'P-0012358-T01-IM5')]['lcn.em.1']
+print phasingData[(phasingData['Tumor_Sample_Barcode'] == 'P-0012358-T01-IM5')]['ccf_Mcopies.1']
+
+print Counter(phasingData[(phasingData['Tumor_Sample_Barcode'] == 'P-0012358-T01-IM5')]['phase'])
+
+sigs = pd.read_table(pathPrefix + '/ifs/work/taylorlab/friedman/myAdjustedDataFiles/hypermutationAnalysisProjectData/hypermutationAnalysisProjectSignatures.tsv')
+sigs['pid'] = sigs['Tumor_Sample_Barcode'].apply(lambda x: x[:9])
+cDict = analysis_utils.get_cancer_type_information(cancerTypeDfPath = pathPrefix +'/ifs/work/taylorlab/friedman/msk-impact/msk-impact/data_clinical_sample.txt')
+sigs['cancer_type'] = sigs['pid'].apply(lambda x: cDict[x] if x in cDict else None)
+
+maf = pd.read_table(pathPrefix + '/ifs/work/taylorlab/friedman/myAdjustedDataFiles/hypermutationAnalysisProjectData/hypermutationAnalysisProjectMaf.tsv')
+
+endometrialIds = set(sigs[sigs['cancer_type'] == 'Endometrial Cancer']['Tumor_Sample_Barcode'])
+colorectalIds = set(sigs[sigs['cancer_type'] == 'Colorectal Cancer']['Tumor_Sample_Barcode'])
+gliomaIds = set(sigs[sigs['cancer_type'] == 'Glioma']['Tumor_Sample_Barcode'])
 
 oneOncMutant = phasingData[(phasingData['oncogenic.1'].notnull()) | (phasingData['oncogenic.2'].notnull())]
 
+oneOncMutant['variant_classification.1']
+
 oneOncMutant.columns.values
+
+endoInfo = oneOncMutant[oneOncMutant['Tumor_Sample_Barcode'].isin(endometrialIds)]
+coloInfo = oneOncMutant[oneOncMutant['Tumor_Sample_Barcode'].isin(colorectalIds)]
 tenMostCommonMuts = Counter(oneOncMutant['Hugo_Symbol']).most_common(10)
-for mut in tenMostCommonMuts:
-    gene = mut[0]
-    geneMuts = oneOncMutant[oneOncMutant['Hugo_Symbol'] == gene]
-    print gene, geneMuts[geneMuts['phase'] != 'unknown']['phase']
+
+
+
+x = []
+for cancerType in ['Endometrial Cancer', 'Colorectal Cancer', 'Glioma']:
+    l = analyze_phasing_amongst_mutations_in_cancer_type(set(sigs[sigs['cancer_type'] == cancerType]['Tumor_Sample_Barcode']),
+                                                 maf, phasingData, cancerType)
+    x = x + l
+df = pd.DataFrame(x)
+df['cancerTypeAndGene'] = df.apply(lambda row: row['cancer_type'] + '_' + row['geneLabel'], axis=1)
+
+df.to_csv('~/Desktop/WORK/dataForLocalPlotting/phasingInfo.tsv', sep='\t', index=False)
 
