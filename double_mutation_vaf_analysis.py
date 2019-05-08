b649bfeef478816cde661dc373b1d8d5568bd6d3
NoahFriedman <friedman@mskcc.org>
2019-05-08 16:27:36 -0400

    added code for looking at double mutation as indicated by vaf

--- double_mutation_vaf_analysis.py
+++ double_mutation_vaf_analysis.py
@@ -16,20 +16,49 @@ sys.path.append(pathPrefix + '/ifs/work/taylorlab/friedman/myUtils')
 import analysis_utils 
 import mutationSigUtils 
 import maf_analysis_utils
+import clonality_analysis_util
 
 #todo build data across cohort case by case
 #note there are different analyses including matrix of relevant/relevant and gene specific bialleic 
-def compare_vafs(maf):
+#compare vafs between pairs of different classes of mutations in a single case
+def compare_vafs(maf, referenceCohortMaf):
+    
+    returnDict = {'allMutations': [], 'allOncogenicMutations':[],
+                  'allRecurrentTumorSuppressors': [], 'oncogenicRecurrentTumorSuppresors': [],
+                  'allRecurrentOncogenes': [], 'oncogenicRecurrentOncogenes': []}
+    
+    recurrentTumorSupressors, recurrentOncogenes = maf_analysis_utils.enumerate_recurrently_mutated_tumor_supressors_and_oncogenes(referenceCohortMaf, thresh=.05)
     oncogenicMutColNames = set(['Likely Oncogenic', 'Oncogenic', 'Predicted Oncogenic'])
     cases = set(maf['Tumor_Sample_Barcode'])
-    diffsOncogenicRates = []
+    genes = set(maf['Hugo_Symbol'])
+    
+    print recurrentOncogenes
+    
     for case in cases:
+        print case
+        #for gene in genes:
+         #   geneMaf = maf[maf['Hugo_Symbol'] == gene]
+            #here are all the cases/areas we look at
         caseMafAllMuts = maf[maf['Tumor_Sample_Barcode'] == case]
+        #caseMafAllMuts = geneMaf[geneMaf['Tumor_Sample_Barcode'] == case]
+        
         caseMafAllOncogenicMuts = caseMafAllMuts[caseMafAllMuts['oncogenic'].isin(oncogenicMutColNames)]
-        diffsAllMuts = analysis_utils.calculate_all_pairwise_differences(np.array(list(caseMafAllMuts['ccf_Mcopies'])))
-        diffsOncogenicMuts = analysis_utils.calculate_all_pairwise_differences(np.array(list(caseMafAllOncogenicMuts['ccf_Mcopies'])))
-        print np.nanmean(diffsAllMuts), np.nanmean(diffsOncogenicMuts)
-
+        caseMafAllReccurentTumorSuppressors = caseMafAllMuts[caseMafAllMuts['Hugo_Symbol'].isin(recurrentTumorSupressors)]
+        caseMafOncogenicRecurrentTumorSuppresors = caseMafAllReccurentTumorSuppressors[caseMafAllReccurentTumorSuppressors['oncogenic'].isin(oncogenicMutColNames)]
+        caseMafAllRecurrentOncogenes = caseMafAllMuts[caseMafAllMuts['Hugo_Symbol'].isin(recurrentOncogenes)]
+        caseMafOncogenicRecurrentOncogenes = caseMafAllRecurrentOncogenes[caseMafAllRecurrentOncogenes['oncogenic'].isin(oncogenicMutColNames)]
+        
+        #add information to our return dict
+        returnDict['allMutations'] = returnDict['allMutations'] + analysis_utils.calculate_all_pairwise_differences(np.array(list(caseMafAllMuts['ccf_Mcopies'])))
+        returnDict['allOncogenicMutations'] = returnDict['allOncogenicMutations'] + analysis_utils.calculate_all_pairwise_differences(np.array(list(caseMafAllOncogenicMuts['ccf_Mcopies'])))
+        returnDict['allRecurrentTumorSuppressors'] = returnDict['allRecurrentTumorSuppressors'] + analysis_utils.calculate_all_pairwise_differences(np.array(list(caseMafAllReccurentTumorSuppressors['ccf_Mcopies'])))
+        returnDict['oncogenicRecurrentTumorSuppresors'] = returnDict['oncogenicRecurrentTumorSuppresors'] + analysis_utils.calculate_all_pairwise_differences(np.array(list(caseMafOncogenicRecurrentTumorSuppresors['ccf_Mcopies'])))
+        returnDict['allRecurrentOncogenes'] = returnDict['allRecurrentOncogenes'] + analysis_utils.calculate_all_pairwise_differences(np.array(list(caseMafAllRecurrentOncogenes['ccf_Mcopies'])))
+        returnDict['oncogenicRecurrentOncogenes'] = returnDict['oncogenicRecurrentOncogenes'] + analysis_utils.calculate_all_pairwise_differences(np.array(list(caseMafOncogenicRecurrentOncogenes['ccf_Mcopies'])))
+        
+    return returnDict
+       
+        
 mafAnnoMaf = pd.read_table(pathPrefix + '/ifs/work/taylorlab/friedman/myAdjustedDataFiles/All.dmp_somatic_data_mutations_unfiltered.mafAnno.oncokb.hotspots.maf', sep='\t')
 mafAnnoMaf['vaf'] = mafAnnoMaf.apply(lambda row: 1.0*row['t_alt_count']/(row['t_alt_count'] + row['t_ref_count']) if row['t_alt_count'] + row['t_ref_count'] > 0 else None, axis = 1)
 
@@ -40,143 +69,87 @@ impactSigs['cancer_type'] = impactSigs['pid'].apply(lambda x: cDict[x] if x in c
 
 hypermutationThresh = 50
 endometrialHyperIds = set(impactSigs[(impactSigs['cancer_type'] == 'Endometrial Cancer') & (impactSigs['Nmut_Mb'] > hypermutationThresh)]['Tumor_Sample_Barcode'])
+endometrialNotHyperIds = set(impactSigs[(impactSigs['cancer_type'] == 'Endometrial Cancer') & (impactSigs['Nmut_Mb'] <= hypermutationThresh)]['Tumor_Sample_Barcode'])
 gliomaHyperIds = set(impactSigs[(impactSigs['cancer_type'] == 'Glioma') & (impactSigs['Nmut_Mb'] > hypermutationThresh)]['Tumor_Sample_Barcode'])
 colorectalHyperIds = set(impactSigs[(impactSigs['cancer_type'] == 'Colorectal Cancer') & (impactSigs['Nmut_Mb'] > hypermutationThresh)]['Tumor_Sample_Barcode'])
+colorectalNotHyperIds = set(impactSigs[(impactSigs['cancer_type'] == 'Colorectal Cancer') & (impactSigs['Nmut_Mb'] <= hypermutationThresh)]['Tumor_Sample_Barcode'])
 
 genes = set(['ABL1', 'ACVR1', 'AGO2', 'AKT1', 'AKT2', 'AKT3', 'ALK', 'ALOX12B', 'ANKRD11', 'APC', 'AR', 'ARAF', 'ARID1A', 'ARID1B', 'ARID2', 'ARID5B', 'ASXL1', 'ASXL2', 'ATM', 'ATR', 'ATRX', 'AURKA', 'AURKB', 'AXIN1', 'AXIN2', 'AXL', 'B2M', 'BABAM1', 'BAP1', 'BARD1', 'BBC3', 'BCL10', 'BCL2', 'BCL2L1', 'BCL2L11', 'BCL6', 'BCOR', 'BIRC3', 'BLM', 'BMPR1A', 'BRAF', 'BRCA1', 'BRCA2', 'BRD4', 'BRIP1', 'BTK', 'CALR', 'CARD11', 'CARM1', 'CASP8', 'CBFB', 'CBL', 'CCND1', 'CCND2', 'CCND3', 'CCNE1', 'CD274', 'CD276', 'CD79A', 'CD79B', 'CDC42', 'CDC73', 'CDH1', 'CDK12', 'CDK4', 'CDK6', 'CDK8', 'CDKN1A', 'CDKN1B', 'CDKN2A', 'CDKN2B', 'CDKN2C', 'CEBPA', 'CENPA', 'CHEK1', 'CHEK2', 'CIC', 'CREBBP', 'CRKL', 'CRLF2', 'CSDE1', 'CSF1R', 'CSF3R', 'CTCF', 'CTLA4', 'CTNNB1', 'CUL3', 'CXCR4', 'CYLD', 'CYSLTR2', 'DAXX', 'DCUN1D1', 'DDR2', 'DICER1', 'DIS3', 'DNAJB1', 'DNMT1', 'DNMT3A', 'DNMT3B', 'DOT1L', 'DROSHA', 'DUSP4', 'E2F3', 'EED', 'EGFL7', 'EGFR', 'EIF1AX', 'EIF4A2', 'EIF4E', 'ELF3', 'EP300', 'EPAS1', 'EPCAM', 'EPHA3', 'EPHA5', 'EPHA7', 'EPHB1', 'ERBB2', 'ERBB3', 'ERBB4', 'ERCC2', 'ERCC3', 'ERCC4', 'ERCC5', 'ERF', 'ERG', 'ERRFI1', 'ESR1', 'ETV1', 'ETV6', 'EZH1', 'EZH2', 'FAM123B', 'FAM175A', 'FAM46C', 'FAM58A', 'FANCA', 'FANCC', 'FAT1', 'FBXW7', 'FGF19', 'FGF3', 'FGF4', 'FGFR1', 'FGFR2', 'FGFR3', 'FGFR4', 'FH', 'FLCN', 'FLT1', 'FLT3', 'FLT4', 'FOXA1', 'FOXL2', 'FOXO1', 'FOXP1', 'FUBP1', 'FYN', 'GATA1', 'GATA2', 'GATA3', 'GLI1', 'GNA11', 'GNAQ', 'GNAS', 'GPS2', 'GREM1', 'GRIN2A', 'GSK3B', 'H3F3A', 'H3F3B', 'H3F3C', 'HGF', 'HIST1H1C', 'HIST1H2BD', 'HIST1H3A', 'HIST1H3B', 'HIST1H3C', 'HIST1H3D', 'HIST1H3E', 'HIST1H3F', 'HIST1H3G', 'HIST1H3H', 'HIST1H3I', 'HIST1H3J', 'HIST2H3C', 'HIST2H3D', 'HIST3H3', 'HLA-A', 'HLA-B', 'HNF1A', 'HOXB13', 'HRAS', 'ICOSLG', 'ID3', 'IDH1', 'IDH2', 'IFNGR1', 'IGF1', 'IGF1R', 'IGF2', 'IKBKE', 'IKZF1', 'IL10', 'IL7R', 'INHA', 'INHBA', 'INPP4A', 'INPP4B', 'INPPL1', 'INSR', 'IRF4', 'IRS1', 'IRS2', 'JAK1', 'JAK2', 'JAK3', 'JUN', 'KDM5A', 'KDM5C', 'KDM6A', 'KDR', 'KEAP1', 'KIT', 'KLF4', 'KMT2B', 'KMT5A', 'KNSTRN', 'KRAS', 'LATS1', 'LATS2', 'LMO1', 'LYN', 'MALT1', 'MAP2K1', 'MAP2K2', 'MAP2K4', 'MAP3K1', 'MAP3K13', 'MAP3K14', 'MAPK1', 'MAPK3', 'MAPKAP1', 'MAX', 'MCL1', 'MDC1', 'MDM2', 'MDM4', 'MED12', 'MEF2B', 'MEN1', 'MET', 'MGA', 'MITF', 'MLH1', 'MLL', 'MLL2', 'MLL3', 'MPL', 'MRE11A', 'MSH2', 'MSH3', 'MSH6', 'MSI1', 'MSI2', 'MST1', 'MST1R', 'MTOR', 'MUTYH', 'MYC', 'MYCL1', 'MYCN', 'MYD88', 'MYOD1', 'NBN', 'NCOA3', 'NCOR1', 'NEGR1', 'NF1', 'NF2', 'NFE2L2', 'NFKBIA', 'NKX2-1', 'NKX3-1', 'NOTCH1', 'NOTCH2', 'NOTCH3', 'NOTCH4', 'NPM1', 'NRAS', 'NSD1', 'NTHL1', 'NTRK1', 'NTRK2', 'NTRK3', 'NUF2', 'NUP93', 'PAK1', 'PAK7', 'PALB2', 'PARK2', 'PARP1', 'PAX5', 'PBRM1', 'PDCD1', 'PDCD1LG2', 'PDGFRA', 'PDGFRB', 'PDPK1', 'PGR', 'PHOX2B', 'PIK3C2G', 'PIK3C3', 'PIK3CA', 'PIK3CB', 'PIK3CD', 'PIK3CG', 'PIK3R1', 'PIK3R2', 'PIK3R3', 'PIM1', 'PLCG2', 'PLK2', 'PMAIP1', 'PMS1', 'PMS2', 'PNRC1', 'POLD1', 'POLE', 'PPARG', 'PPM1D', 'PPP2R1A', 'PPP4R2', 'PPP6C', 'PRDM1', 'PRDM14', 'PREX2', 'PRKAR1A', 'PRKCI', 'PRKD1', 'PTCH1', 'PTEN', 'PTP4A1', 'PTPN11', 'PTPRD', 'PTPRS', 'PTPRT', 'RAB35', 'RAC1', 'RAC2', 'RAD21', 'RAD50', 'RAD51', 'RAD51C', 'RAD51L1', 'RAD51L3', 'RAD52', 'RAD54L', 'RAF1', 'RARA', 'RASA1', 'RB1', 'RBM10', 'RECQL', 'RECQL4', 'REL', 'RET', 'RFWD2', 'RHEB', 'RHOA', 'RICTOR', 'RIT1', 'RNF43', 'ROS1', 'RPS6KA4', 'RPS6KB2', 'RPTOR', 'RRAGC', 'RRAS', 'RRAS2', 'RTEL1', 'RUNX1', 'RXRA', 'RYBP', 'SDHA', 'SDHAF2', 'SDHB', 'SDHC', 'SDHD', 'SESN1', 'SESN2', 'SESN3', 'SETD2', 'SF3B1', 'SH2B3', 'SH2D1A', 'SHOC2', 'SHQ1', 'SLX4', 'SMAD2', 'SMAD3', 'SMAD4', 'SMARCA4', 'SMARCB1', 'SMARCD1', 'SMO', 'SMYD3', 'SOCS1', 'SOS1', 'SOX17', 'SOX2', 'SOX9', 'SPEN', 'SPOP', 'SPRED1', 'SRC', 'SRSF2', 'STAG2', 'STAT3', 'STAT5A', 'STAT5B', 'STK11', 'STK19', 'STK40', 'SUFU', 'SUZ12', 'SYK', 'TAP1', 'TAP2', 'TBX3', 'TCEB1', 'TCF3', 'TCF7L2', 'TEK', 'TERT', 'TET1', 'TET2', 'TGFBR1', 'TGFBR2', 'TMEM127', 'TMPRSS2', 'TNFAIP3', 'TNFRSF14', 'TOP1', 'TP53', 'TP53BP1', 'TP63', 'TRAF2', 'TRAF7', 'TSC1', 'TSC2', 'TSHR', 'U2AF1', 'UPF1', 'VEGFA', 'VHL', 'VTCN1', 'WHSC1', 'WHSC1L1', 'WT1', 'WWTR1', 'XIAP', 'XPO1', 'XRCC2', 'YAP1', 'YES1', 'ZFHX3', 'ZRSR2'])
 oncogenicMutColNames = set(['Likely Oncogenic', 'Oncogenic', 'Predicted Oncogenic'])
 
 mafAnnoMafWithData = mafAnnoMaf[mafAnnoMaf['ccf_Mcopies'].notnull()]
+mafAnnoMafWithData['pid'] = mafAnnoMafWithData['Tumor_Sample_Barcode'].apply(lambda x: x[:9])
 
 #cohortMaf = mafAnnoMaf[mafAnnoMaf['Tumor_Sample_Barcode'].isin(endometrialHyperIds) & (mafAnnoMaf['oncogenic'].isin(oncogenicMutColNames))]
 #cohortMaf = mafAnnoMaf[mafAnnoMaf['Tumor_Sample_Barcode'].isin(endometrialHyperIds)]
 #cohortMaf = mafAnnoMaf[mafAnnoMaf['Tumor_Sample_Barcode'].isin(gliomaHyperIds)]
-cohortMaf = mafAnnoMafWithData[mafAnnoMafWithData['Tumor_Sample_Barcode'].isin(colorectalHyperIds)]
-
-compare_vafs(cohortMaf)
-
-cohortMaf.columns.values
+cohortMaf = mafAnnoMafWithData[mafAnnoMafWithData['Tumor_Sample_Barcode'].isin(endometrialHyperIds)]
 
 
+#####
+#########
+##############
+#################   NEW VERSION   ########### 
 
+#TODO check for genome doubling
+def create_two_hit_mutation_summary(df, alleleCountThresh =5, doubleThresh=1.75):
+    
+    listOfDicts = []
+    #PROCESS THE MAF AS NEEDED
+    df = maf_analysis_utils.fix_mll_genes(df)
+    df = maf_analysis_utils.mark_cases_with_flat_genomes(df)
+    df = maf_analysis_utils.mark_cases_with_median_vaf_of_case(df)
+    df = df[df['oncogenic'].notnull()]
+    df = df[df['HGVSp_Short'].notnull()]
+    
+    df['isDouble'] = df.apply(lambda row: clonality_analysis_util.is_mut_double_hit(row, row['medianVaf'], row['FlatGenome'], doubleThresh), axis=1)
+    
+    dictOfMuts = dict(Counter(df[df['isDouble'] == True]['Hugo_Symbol']))
+    for gene, count in dictOfMuts.items():
+        if count > 1:
+            for allele, aCount in Counter(df[(df['Hugo_Symbol'] == gene) & (df['isDouble'] == True)]['HGVSp_Short']).items():
+                indel = False
+                if '*' in allele: indel = True
+                d = {'Gene': gene, 'Allele': allele, 'AlleleCount': aCount, 'count': count, 'orderingVal': count, 'indel':indel}
+                listOfDicts.append(d)
+        else: #PROCESS DATA SEPARATELY FOR ONE OFF ALELLES
+            allele = df[(df['Hugo_Symbol'] == gene) & (df['isDouble'] == True)].iloc[0]['HGVSp_Short']
+            indel = False
+            if '*' in allele: indel = True
+            d = {'Gene': 'other', 'Allele': allele, 'AlleleCount': 1, 'count': 1, 'orderingVal': -1, 'indel':indel}
+            listOfDicts.append(d)
+    
+    df = pd.DataFrame(listOfDicts)
+    df['label'] = df.apply(lambda row: row['Allele'] if row['AlleleCount'] >= alleleCountThresh and row['Allele'] != 'various' else None, axis=1)
+    return df
+    
+#ANALYZE COHORTS TO SEE PREVALENCE AND SPECIFICITY OF DOUBLE HIT MUTS
 
+colorectalDf = pd.read_table(pathPrefix + '/ifs/work/taylorlab/friedman/myAdjustedDataFiles/subsettedMafs/Colorectal_HypermutantCaseMuts_MAF_ANNO.maf')
+endometrialDf = pd.read_table(pathPrefix + '/ifs/work/taylorlab/friedman/myAdjustedDataFiles/subsettedMafs/Endometrial_HypermutantCaseMuts_MAF_ANNO.maf')
+gliomaDf = pd.read_table(pathPrefix + '/ifs/work/taylorlab/friedman/myAdjustedDataFiles/subsettedMafs/Glioma_HypermutantCaseMuts_MAF_ANNO.maf')
 
+#TCF7L2--9mer, RNF43---7mer MSH3 AAAAAAA homopolymer, PTEN 6 homopolymer
+    #APC  p.T1556Nfs*3--6mer, p.R856Nfs*6: 3mer
 
+endoSummary = create_two_hit_mutation_summary(endometrialDf)
+endoSummary.to_csv('~/Desktop/WORK/dataForLocalPlotting/endometrialDoubleHitPlotting.tsv', index=False, sep='\t')
 
+colorectalSummary =  create_two_hit_mutation_summary(colorectalDf)
+colorectalSummary.to_csv('~/Desktop/WORK/dataForLocalPlotting/colorectalDoubleHitPlotting.tsv', index=False, sep='\t')
 
+gliomaSummary =  create_two_hit_mutation_summary(gliomaDf)
+gliomaSummary.to_csv('~/Desktop/WORK/dataForLocalPlotting/gliomaDoubleHitPlotting.tsv', index=False, sep='\t')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-#genes = set(['PTEN', 'PIK3CA', 'ARID1A', 'NF1', 'JAK1', 'RASA1', 'CIC', 'ATM'])
-d = {}
-cntr =0
-for gene in genes:
-    print cntr
-    cntr += 1
-    geneMaf = cohortMaf[cohortMaf['Hugo_Symbol'] == gene]
-    l = []
-    for case in set(geneMaf['Tumor_Sample_Barcode']):
-        caseMafAllMuts = cohortMaf[cohortMaf['Tumor_Sample_Barcode'] == case]
-        maxVaf = max(caseMafAllMuts['vaf'])
-        caseGeneMaf = geneMaf[geneMaf['Tumor_Sample_Barcode'] == case]
-        if caseGeneMaf.shape[0] > 1:
-            vafs = list(caseGeneMaf['vaf'])
-            minDist = min([abs(x - y) for i,x in enumerate(vafs) for j,y in enumerate(vafs) if i != j])
-            minDistAsPct = minDist/maxVaf
-            l.append(minDistAsPct)
-    d[gene] = l
-     
-for gene in genes:
-    if len(d[gene]) > 5:
-        print gene, np.nanmedian(d[gene]), len(d[gene])   
-        
-###############################3333
 print 
 
-
-mutationMaf = pd.read_table(pathPrefix + '/ifs/work/taylorlab/friedman/myAdjustedDataFiles/hypermutationAnalysisProjectData/hypermutationAnalysisProjectMaf.tsv')
-
-print impactSigs[impactSigs['Nmut_Mb'] > 100].shape[0]/34744.0
-
-impactSigs = mutationSigUtils.merge_signature_columns(impactSigs, mode='Stratton', drop=True, smokingMerge=True, confidence=True, mean=True, prefix='mean_')
-impactSigs['dominantSignature'] = impactSigs.apply(lambda row: mutationSigUtils.get_dominant_signature(row.to_dict()), axis=1)
-
-sigsToMakePlotWith = impactSigs[impactSigs['dominantSignature'].isin(set(['mean_1', 'mean_3', 'mean_SMOKING', 'mean_APOBEC', 'mean_7', 'mean_10', 'mean_MMR', 'mean_11', 'mean_14']))]
-sigsToMakePlotWith = sigsToMakePlotWith[(sigsToMakePlotWith['Nmut'] > 10)]
-sigsToMakePlotWith['orderingVal'] = sigsToMakePlotWith['dominantSignature'].apply(lambda x: 
-    1 if x == 'mean_10'
-    else 2 if x == 'mean_14'
-    else 3 if x == 'mean_MMR'
-    else 4 if x == 'mean_11'
-    else 5 if x == 'mean_7'
-    else 6 if x == 'mean_APOBEC'
-    else 7 if x == 'mean_SMOKING'
-    else 8
-    )
-sigsToMakePlotWith['dominantSignature'] = sigsToMakePlotWith['dominantSignature'].apply(lambda x:
-    'POLE' if x == 'mean_10'
-    else 'POLE & MMR' if x == 'mean_14'
-    else 'MMR' if x == 'mean_MMR'
-    else 'TMZ' if x == 'mean_11'
-    else 'UV' if x == 'mean_7'
-    else 'APOBEC' if x == 'mean_APOBEC'
-    else 'SMOKING' if x == 'mean_SMOKING'
-    else 'BRCA' if x == 'mean_3'
-    else 'AGING' if x == 'mean_1'
-    else x
-    )
-sigsToMakePlotWith.to_csv('~/Desktop/WORK/dataForLocalPlotting/signatureSummary.tsv', index=False, sep='\t')
+################3
+gliomaDf2 = maf_analysis_utils.mark_cases_with_median_vaf_of_case(gliomaDf)
+gliomaDf2 = gliomaDf2[gliomaDf2['oncogenic'].notnull()]
+gliomaDf2 = gliomaDf2[gliomaDf2['HGVSp_Short'].notnull()]
+gliomaDf2['isDouble'] = gliomaDf2.apply(lambda row: clonality_analysis_util.is_mut_double_hit(row, row['medianVaf'], row['FlatGenome'], 1.75), axis=1)
+    
+for i in gliomaDf2[(gliomaDf2['isDouble'] == True) & (gliomaDf['Hugo_Symbol'] == 'IDH1')]['t_var_freq']:
+    print i
 
 
-################33
-mmrDominant = set(impactSigs[(impactSigs['dominantSignature'] == 'mean_MMR') & (impactSigs['Nmut'] > 10)]['Tumor_Sample_Barcode'])
-poleDominant = set(impactSigs[((impactSigs['dominantSignature'] == 'mean_10') | (impactSigs['dominantSignature'] == 'mean_14')) &(impactSigs['Nmut_Mb'] > 10)]['Tumor_Sample_Barcode'])
-mutationMafMMR = mutationMaf[mutationMaf['Tumor_Sample_Barcode'].isin(mmrDominant)]
-mutationMafMMR['Variant_Type']
-
-dfBurdenInfo = maf_analysis_utils.summarize_per_case_mutation_info_for_mafs(mutationMaf)
-
-dfBurdenInfo['oncogenicToNmutRatio'] = dfBurdenInfo.apply(lambda row: None if row['Nmut'] == 0 else 1.0*row['nOncogenicMuts']/row['Nmut'], axis=1)
-
-l = []
-for sigName in ['mean_MMR', 'mean_APOBEC', 'mean_10', 'mean_11', 'mean_7']:
-    ids = set(impactSigs[((impactSigs['dominantSignature'] == sigName)) &(impactSigs['Nmut_Mb'] > 100)]['Tumor_Sample_Barcode'])
-    ratio = np.nanmean(dfBurdenInfo[dfBurdenInfo['Tumor_Sample_Barcode'].isin(ids)]['oncogenicToNmutRatio'])
-    caseMaf = mutationMaf[mutationMaf['Tumor_Sample_Barcode'].isin(ids)]
-    indelFrac = 1.0*caseMaf[(caseMaf['Variant_Type'] == 'INS') | (caseMaf['Variant_Type'] == 'DEL')].shape[0]/caseMaf.shape[0]
-    l.append({'sigName': sigName, 'ratio':ratio, 'indelFrac': indelFrac})
-df = pd.DataFrame(l)
-df.to_csv('~/Desktop/WORK/dataForLocalPlotting/indelAndSusceptibility.tsv', index=False, sep='\t')
-
-
-print np.nanmean(dfBurdenInfo[dfBurdenInfo['Tumor_Sample_Barcode'].isin(poleDominant)]['oncogenicToNmutRatio'])
-
-
-
-nMMRMutations = mutationMafMMR.shape[0]
-nMMROncogenicMutations = mutationMafMMR[mutationMafMMR['oncogenic'].isin(oncogenicMutColNames)].shape[0]
-
-
-print mutationMafMMR[(mutationMafMMR['Variant_Type'] == 'INS') | (mutationMafMMR['Variant_Type'] == 'DEL')].shape
-print mutationMafMMR[(mutationMafMMR['Variant_Type'] == 'SNP')].shape
-
-
-
-
-print mutationMafMMR[((mutationMafMMR['Variant_Type'] == 'INS') | (mutationMafMMR['Variant_Type'] == 'DEL')) & (mutationMafMMR['oncogenic'].isin(oncogenicMutColNames))].shape
-
-print mutationMafMMR[((mutationMafMMR['Variant_Type'] == 'SNP')) & (mutationMafMMR['oncogenic'].isin(oncogenicMutColNames))].shape
-
-
-print mutationMafMMR[mutationMafMMR['oncogenic'].isin(oncogenicMutColNames)]
