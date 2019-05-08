2f80060c41e7ae13b5df7f1cabf1f3587a86794e
NoahFriedman <friedman@mskcc.org>
2019-05-08 16:29:19 -0400

    more code and info here

--- plottingScripts/plot_gene_mut_susceptibility.R
+++ plottingScripts/plot_gene_mut_susceptibility.R
@@ -7,6 +7,8 @@ require(cowplot)
 library(egg)
 library(dplyr)
 library(data.table); setDTthreads(6)
+library(ggrepel)
+library(RColorBrewer) 
 
 if(!exists("foo", mode="function")) source("/Users/friedman/Desktop/mnt/ifs/work/taylorlab/friedman/myUtils/landscape_plot_util.R")
 
@@ -38,8 +40,63 @@ quadNucMutSusceptibilityPlot <- function(df, title=''){
   return(plt)
 }
 
+plot_expected_mutations_by_signature_aetiology<- function(df){
+  plt<- ggplot(df, aes(x=Nmut_Mb, y=Nmut_Expected, colour=Signature, alpha=colorOpaque)) +
+    geom_line()+
+    scale_x_log10()+
+    ggtitle('Oncogenicty of Signatures')+
+    ylab('N Expected Oncogenic Mutations')+
+    theme(legend.position="none")+
+    labs(caption="data:compare_hypothetical_mutability_by_signature.py, plot_gene_mut_susceptibility.R")
+  return(plt)
+}
+
+plot_observed_mutations_by_signature_aetiology<- function(df){
+ palette = c( 
+  "#00DFFF", #age is 
+  "#FF0000", #apobec is red
+  "#FF1493", #BRCA sig deep pink
+  "#267574", #mmr is blue-green
+  "gray", #other is gray
+  "#ADFF2F", #Pole is
+  "#551A8B", #Pole_MMR
+  "#FFA500", #smoking is orange
+  "purple", #tmz
+  "#FFF600" #UV is
+  )
+  plt<- ggplot(df, aes(x=Nmut_Mb, y=oncogenicMutsPerCase, colour=dominantSigAdj)) +
+    #ggplot(df, aes(x=Nmut_Mb, y=oncogenicMutsPerCase, group =dominantSigAdj, colour=sigDisplayLabel)) +
+    geom_smooth(method='loess', formula = y ~ x, alpha=0.25)+
+    coord_cartesian(xlim = c(10,1000))+
+    scale_x_log10()+
+    ggtitle('Oncogenicty of Signatures')+
+    ylab('N Observed Oncogenic Mutations')
+    #scale_colour_manual(values = palette)
+    #theme(legend.position="none")+
+    #labs(caption="data:compare_hypothetical_mutability_by_signature.py, plot_gene_mut_susceptibility.R")
+  return(plt)
+}
 
 
+###CURRENT CODE
+#PLOT expected curves
+df <- read.table('~/Desktop/WORK/dataForLocalPlotting/expectedOncogenicDataBySig.tsv', sep = '\t', header=TRUE)
+plt <- plot_expected_mutations_by_signature_aetiology(df)
+ggsave('~/Desktop/plot.pdf', plot=plt,  width = 3, height = 4, units = c("in"), limitsize = FALSE)
+
+#PLOT observed curves
+df <- read.table('~/Desktop/WORK/dataForLocalPlotting/observedOncogenicDataBySig.tsv', sep='\t', header=TRUE)
+plt <- plot_observed_mutations_by_signature_aetiology(df)
+ggsave('~/Desktop/plot.pdf', plot=plt,  width = 5, height = 4, units = c("in"), limitsize = FALSE)
+
+
+
+
+
+
+
+
+##########OLDER CODE
 #####STUFF WITH FUNCTIONS
 
 
