9617b6440440f24ab6270e0c15f06d37400dc786
NoahFriedman <friedman@mskcc.org>
2019-10-04 12:25:59 -0400

    added new plots

--- plottingScripts/plot_Vaf_Info.R
+++ plottingScripts/plot_Vaf_Info.R
@@ -24,6 +24,20 @@ plot_vaf_info_with_error_bars <- function(df, title=''){
   return(plt)
 }
 
+plot_clonality_info_with_error_bars2 <- function(df, title=''){
+  plt <- ggplot(df, aes(x = type, y = ccfInfo, colour=cancerType))+
+    geom_segment(aes(y = lower, yend=upper, xend=type))+ #x error bar
+    geom_point()+
+    ylab('CCF')+
+    xlab('type')+
+    ggtitle(title)+
+    theme(axis.text.x = element_text(angle = 60, hjust = 1, size=8))+
+    xlab('')
+    #theme(plot.title = element_text(hjust = 0.5))
+  return(plt)
+}
+
+
 df <- read.table('~/Desktop/WORK/dataForLocalPlotting/colorectalVafInfo.tsv',sep = '\t', header=TRUE)
 coloPlot <- plot_vaf_info_with_error_bars(df, title='Hypermutated Colorectal Cancer')
 ggsave('~/Desktop/plot.pdf', plot=coloPlot,  width = 6, height = 6, units = c("in"))
@@ -32,4 +46,24 @@ ggsave('~/Desktop/plot.pdf', plot=coloPlot,  width = 6, height = 6, units = c("i
 df <- read.table('~/Desktop/WORK/dataForLocalPlotting/endometrialVafInfo.tsv',sep = '\t', header=TRUE)
 endoPlot <- plot_vaf_info_with_error_bars(df, title='Hypermutated Endometrial Cancer')
 ggsave('~/Desktop/plot.pdf', plot=endoPlot,  width = 6, height = 6, units = c("in"))
+
+
+
+################
+df <- read.table('/Users/friedman/Desktop/WORK/dataForLocalPlotting/hypermutationCohortMutationClonality.tsv',sep = '\t', header=TRUE)
+plt <- plot_clonality_info_with_error_bars2(df)
+caption <- ggplot()+labs(caption='data=summarize_nmut_and_noncogenic_info_for_mutation_simulation.ipynb, plot=plot_Vaf_Info.R')
+p2 <- plot_grid(plt, caption, nrow=2, rel_heights = c(1,.1))
+
+
+
+
+df <- read.table('~/Desktop/WORK/dataForLocalPlotting/anecdoteMuts.tsv', sep = '\t', header=TRUE)
+
+ggplot(df, aes(x=ccf, fill=label)) +
+  geom_histogram(position="identity")
+
+
+
+
 
