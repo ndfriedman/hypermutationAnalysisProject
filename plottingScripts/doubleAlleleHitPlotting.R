187b26f2b2367137ade63a484e1c6db8f0042636
NoahFriedman <friedman@mskcc.org>
2019-10-04 12:25:38 -0400

    added a new way to plot

--- plottingScripts/doubleAlleleHitPlotting.R
+++ plottingScripts/doubleAlleleHitPlotting.R
@@ -16,7 +16,18 @@ if(!exists("foo", mode="function")) source("/Users/friedman/Desktop/mnt/ifs/work
 #pal <- colorRampPalette(brewer.pal(8,"Set1"))
 #scale_colour_manual(values = pal(palSize))
 
+#plt <- plt +labs(caption = "data:double_mutation_vaf_analysis.py; plotting: doubleAlleleHitPlotting.R")
+
+emptyTheme <- theme(axis.line = element_blank(),
+                      axis.title = element_blank(),
+                      panel.grid.major = element_blank(),
+                      panel.grid.minor = element_blank(),
+                      panel.border = element_blank(),
+                      panel.background = element_blank())
+
 plot_double_mut_distribution <- function(df, title='', palSize =40){
+  
+  
   plt <- ggplot(df, aes(x= reorder(Gene, -orderingVal), y=AlleleCount))+
     geom_bar(aes(fill=indel), colour='white', stat='identity')+
     theme(axis.text.x = element_text(angle = 60, hjust = 1, size=10))+
@@ -27,12 +38,14 @@ plot_double_mut_distribution <- function(df, title='', palSize =40){
                     segment.size  = 1,
                     segment.color = "grey50",
                     direction     = "both")+
+    emptyTheme+
     xlab('Gene')+
     ylab('Count')+
     ggtitle(title)
-  plt <- plt +labs(caption = "data:double_mutation_vaf_analysis.py; plotting: doubleAlleleHitPlotting.R")
   return(plt)
 }
+
+
 
 endometrialDf <- read.table('~/Desktop/WORK/dataForLocalPlotting/endometrialDoubleHitPlotting.tsv',sep = '\t', header=TRUE)
 endoPlot <- plot_double_mut_distribution(endometrialDf, title='Endometrial Cancer', palSize = 40)
@@ -40,18 +53,43 @@ endoPlot <- plot_double_mut_distribution(endometrialDf, title='Endometrial Cance
 colorectalDf <- read.table('~/Desktop/WORK/dataForLocalPlotting/colorectalDoubleHitPlotting.tsv',sep = '\t', header=TRUE)
 coloPlot <- plot_double_mut_distribution(colorectalDf, title='Colorectal Cancer', palSize = 60)
 
+alignedPlot <- plot_grid(endoPlot, coloPlot, ncol=2)
+alignedPlotFinal <- plot_grid(ggplot()+ggtitle('Mutations violating infinite sites hypothesis in cancer')+theme(plot.title = element_text(size = 15, face = "bold")),
+                              alignedPlot,
+                              ggplot()+labs(caption='mutations_violating_the_infinite_sites_model.ipynb  doubleAlleleHitPlotting.R'),
+                              nrow=3, rel_heights = c(.1,1,.05))
+
 gliomaDf <- read.table('~/Desktop/WORK/dataForLocalPlotting/gliomaDoubleHitPlotting.tsv',sep = '\t', header=TRUE)
 gliomaPlot <- plot_double_mut_distribution(gliomaDf, title='Glioma')
 
-ggsave('~/Desktop/plot.pdf', plot=coloPlot,  width = 6, height = 4, units = c("in"))
+ggsave('~/Desktop/plot.pdf', plot=alignedPlotFinal,  width = 10, height = 5, units = c("in"))
 
+##########OTHER small plots
 
+#TUMOR sup vs oncogene
+df <- read.table('~/Desktop/WORK/dataForLocalPlotting/combinedDoubleHitPlotting.tsv',sep = '\t', header=TRUE)
+p <- ggplot(df, aes(x=mutType))+
+  geom_bar(stat='count')+
+  emptyTheme+
+  theme(plot.title = element_text(hjust = 0.5, face='bold'))+
+  ggtitle('MVISH mutations')+
+  ylab('N muts')+
+  xlab('Gene type')+
+  theme(axis.text.x = element_text(angle=70, hjust=1))
 
+ggsave('~/Desktop/plot.pdf', plot=p,  width = 2, height = 3, units = c("in"))
 
+##########################
 
-
-df <- read.table('~/Desktop/WORK/dataForLocalPlotting/endometrialDoubleMutationPlotting.tsv',sep = '\t', header=TRUE)
-randoPlot <- plot_double_mut_distribution(df, title='Endometrial Cancer', palSize = 40)
-
+df <- read.table('/Users/friedman/Desktop/WORK/dataForLocalPlotting/MVISH_by_motif.tsv',sep = '\t', header=TRUE)
+p <- ggplot(df, aes(x = reorder(motif, orderingVal)))+
+  geom_bar(stat='count')+
+  emptyTheme+
+  theme(plot.title = element_text(hjust = 0.5, face='bold'))+
+  ggtitle('MVISH mutations')+
+  ylab('N muts')+
+  xlab('Mutation Context')+
+  theme(axis.text.x = element_text(angle=70, hjust=1))
+ggsave('~/Desktop/plot.pdf', plot=p,  width = 2, height = 3, units = c("in"))
 
 
